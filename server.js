a4dc6eb8d5dfd92703ea4133513059e2da674265
kgentel <katie.m.gentel@gmail.com>
2021-06-07 00:11:20 -0500

    connected front and back ends and function to add tasks

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
 const express = require('express');
+const { futimesSync } = require('fs');
+const path = require('path');
 const app = express();
-const db  = require('./db/db');
+const notes  = require('./db/db');
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+function createNewTask(body, tasksArray) {
+    const task = body;
+    tasksArray.push(task);
+
+    return task;
+}
 
 
 
-app.get('/api/db', (req, res) => {
-    res.json(db);
+
+
+
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    
+    const task = createNewTask(req.body, notes);
+    res.json(task);
 });
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
